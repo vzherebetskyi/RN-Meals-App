e27fa9dd7597b548c4519ee56feafc89caa9c87b
vzherebetskyi <vzherebetskyi@gmail.com>
2020-10-19 15:08:59 +0300

    Set the filters screen

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,5 +1,5 @@
 import { MEALS } from '../../data/dummy-data';
-import { TOGGLE_FAVORITE } from '../actions/meals';
+import { TOGGLE_FAVORITE, SET_FILTERS } from '../actions/meals';
 
 const initialMealsState = {
   meals: MEALS,
@@ -18,6 +18,24 @@ const mealsReducer = (state = initialMealsState, action) => {
       } else {
         return { ...state, favoriteMeals: state.favoriteMeals.concat(state.meals.find(meal => meal.id === action.mealId))};
       }
+    case SET_FILTERS:
+      const appliedFilters = action.filters;
+      const filteredMeals = state.meals.filter(meal => {
+        if (appliedFilters.glutenFree && !meal.isGlutenFree) {
+          return false;
+        }
+        if (appliedFilters.lactoseFree && !meal.isLactoseFree) {
+          return false;
+        }
+        if (appliedFilters.vegetarian && !meal.isVegetarian) {
+          return false;
+        }
+        if (appliedFilters.vegan && !meal.isVegan) {
+          return false;
+        }
+        return true;
+      });
+      return { ...state, filteredMeals }
     default:
       return state;
   }
